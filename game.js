f5aa782c5dd430ea915cf072ca9c1c1a5ef159c4
Rob Good <robgood20@gmail.com>
2016-01-13 14:24:23 -0700

    healthbar

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
 var health = 100;
 function slap(){
     health = health-1;
-    alert(health);
+   
 }
 
+var update = document.getElementById('playerhealth');
+function updateHealth(){
+    update.innerText = health.toString()
+    
+}
+updateHealth()
