60e2140da790d97dd2e09be37cf0c33305f4c18c
Rob Good <robgood20@gmail.com>
2016-01-13 15:35:52 -0700

    update

--- game.js
+++ game.js
@@ -2,31 +2,30 @@ var health = 100;
 var playername = "Player 1";
 var playerhits = 0;
 
-function slap(){
-    health = health-1;
-    updateHealth()
-}
-
-function hits(){
-    health = health+1;
-    updateHealth()
-}
-
 var update = document.getElementById('playerhealth');
 function updateHealth(){
-    update.innerText = health.toString()
-    
+    update.innerText = health.toString()    
+}
+
+
+function slap(){
+    health = health-1;
+    hits = hits+1;
+    updateHealth()
 }
 
 function punch(){
     health = health-5;
+    hits = hits+1;
     updateHealth()
 }
 
 function kick(){
     health = health-10;
+    hits = hits+1;
     updateHealth() 
 }
     
-
+updateName()
+updateHits()
 updateHealth()
