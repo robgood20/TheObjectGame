d70e658c5dc42a4ce8a0c41a256d986b443c10bb
Rob Good <robgood20@gmail.com>
2016-01-14 16:44:14 -0700

    add step 4

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var player={
     playerHealth : 100,
     playerName : "Player 1",
     playerHits : 0,
-    items:[itemObject.shield]
+    items:[itemObject.shield, itemObject.armor, itemObject.helmet]
 }
 
 function slap(){
